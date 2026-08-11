5c4b2d0ff27ff7ad541b1356ea909d2c8ff8bbcc
Dev Rastogi <dev.rastogi.334@gmail.com>
2026-08-11 12:56:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-    res.send("Hello World! Express Server is Running.");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
